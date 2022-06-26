9204210152d0ccfd6eb4952e7d14ff432e784976
Prashant Dhote <99156547+prashant744@users.noreply.github.com>
2022-06-26 15:39:13 +0530

    Add Js

---
+++ prashant.js
@@ -0,0 +1,20 @@
+var navMenuAnchorTags=document.querySelectorAll('.no-list-style a');
+
+for(var i=0;i<navMenuAnchorTags.length;i++){
+    navMenuAnchorTags[i].addEventListener('click',function(event){
+    event.preventDefault();
+
+    var targetSectionID=this.textContent.trim().toLowerCase();
+    var targetSection=document.getElementById(targetSectionID);
+    console.log(targetSection);
+
+    var interval= setInterval(function(){
+        var targetSectionCoordinates=targetSection.getBoundingClientRect();
+        if(targetSectionCoordinates.top<=0){
+            clearInterval(interval);
+            return;
+        }
+        window.scrollBy(0,50);
+    },20);
+    });
+}

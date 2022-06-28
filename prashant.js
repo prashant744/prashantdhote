8d97ef4cf21eb5eb1c7aa25568e5f73c18ff65a0
Prashant Dhote <99156547+prashant744@users.noreply.github.com>
2022-06-28 13:10:53 +0530

    Add Js

--- prashant.js
+++ prashant.js
@@ -18,3 +18,62 @@ for(var i=0;i<navMenuAnchorTags.length;i++){
     },20);
     });
 }
+
+
+
+        // skill progress
+
+        var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+        function initialiseBar(bar) {
+            bar.setAttribute("data-visited", false);
+            bar.style.width = 0 + '%';
+        }
+        
+        for (var bar of progressBars) {
+            initialiseBar(bar);
+        }
+        
+        
+        
+        function fillBar(bar) {
+        
+            var currentWidth = 0;
+            var targetWidth = bar.getAttribute("data-bar-width");
+            var interval = setInterval(function () {
+                if (currentWidth >= targetWidth) {
+                    clearInterval(interval);
+                    return;
+                }
+                currentWidth++;
+                bar.style.width = currentWidth + '%';
+            }, 5);
+        
+        }
+        
+        
+        
+        // This function uses a for loop for individual progress bars.
+        function checkScroll() {
+    
+            for (let bar of progressBars) {
+                var barCoordinates = bar.getBoundingClientRect();
+                if ((bar.getAttribute("data-visited") == "false") &&
+                    (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+                    bar.setAttribute("data-visited", true);
+                    fillBar(bar);
+                } else if (barCoordinates.top > window.innerHeight) {
+                    bar.setAttribute("data-visited", false);
+                    initialiseBar(bar);
+                }
+        
+            }
+        }
+        
+        
+        
+        window.addEventListener("scroll", checkScroll);
+        
+        
